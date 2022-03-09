e0cc870a2291667ad37319fbd7f372d3863a8817
Dajve Green <david.green@klevu.com>
2022-03-09 12:22:12 +0000

    Release of version 2.4.2

---
+++ Service/ThemeV2/InteractiveOptionsProvider/AddPriceSuffixToQueryOptionsProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Klevu\Categorynavigation\Service\ThemeV2\InteractiveOptionsProvider;
+
+use Klevu\FrontendJs\Api\InteractiveOptionsProviderInterface;
+use Klevu\FrontendJs\Api\IsEnabledConditionInterface;
+
+class AddPriceSuffixToQueryOptionsProvider implements InteractiveOptionsProviderInterface
+{
+    /**
+     * @var IsEnabledConditionInterface
+     */
+    private $isEnabledCondition;
+
+    /**
+     * @param IsEnabledConditionInterface $isEnabledCondition
+     */
+    public function __construct(
+        IsEnabledConditionInterface $isEnabledCondition
+    ) {
+        $this->isEnabledCondition = $isEnabledCondition;
+    }
+
+    /**
+     * @param int $storeId
+     * @return array
+     */
+    public function execute($storeId = null)
+    {
+        if (!$this->isEnabledCondition->execute($storeId)) {
+            return [];
+        }
+
+        return [
+            'powerUp' => [
+                'catnav' => false,
+            ],
+        ];
+    }
+}
